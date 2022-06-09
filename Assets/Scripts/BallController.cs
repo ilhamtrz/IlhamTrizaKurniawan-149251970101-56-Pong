9ac715a5cb507278b9e8fda6589288993e94eb6c
Ilham Triza Kurniawan <95692045+ilhamtrz@users.noreply.github.com>
2022-06-09 20:04:42 +0700

    Adding UI & Game Manager

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,14 +6,16 @@ public class BallController : MonoBehaviour
 {
     public Vector2 speed;
     private Rigidbody2D rig;
+    public Vector2 resetPosition;
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         rig.velocity = speed;
     }
-    private void Update()
+    
+    public void ResetBall()
     {
-        
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
     }
 }
